4a6ee106c10d93480df83e899ebea088c59a8e0b
Shreya Agrawal <shreya.22b0121006@abes.ac.in>
2024-10-16 15:37:10 +0530

    added JS Work

---
+++ Js Work/shreya.js
@@ -0,0 +1,22 @@
+// function aa(){
+//     console.log("Hello");
+// }
+// aa();
+
+// (function (){
+//     console.log("Hello");
+// })();
+
+
+// ()=>{
+//     console.log("third");
+// }
+
+// let a= new Function('console.log("Hello");');
+
+function rollNumber(num,dalay,nextRoll){
+    setTimeout(()=>{
+        
+    })
+}
+
